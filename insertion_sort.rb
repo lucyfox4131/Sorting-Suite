22ab9ec945cd2dcda059029b33fd944812cd0f01
Lucy Fox <lucyreneefox@gmail.com>
2016-04-03 17:38:34 -0600

    Some more work/Fixed naming errors in test

---
+++ insertion_sort.rb
@@ -0,0 +1,19 @@
+class InsertionSort
+
+  def initialize
+    @array = []
+  end
+
+  def sort(array = @array)
+    new_array = array.shift
+    
+  end
+
+end
+
+
+
+
+#very first thing I am doing is taking the first element
+#in the array given and putting it into our new array,
+#now I'll have to loop through
--- insertion_sort_test.rb
+++ insertion_sort_test.rb
@@ -1,45 +1,38 @@
 require "minitest/autorun"
 require "minitest/pride"
-require './insert_sort'
+require './insertion_sort'
 
-class insertSortTest < Minitest::Test
+class InsertionSortTest < Minitest::Test
 
 #get tips on pulling out a helper class for testing so don't always have to instantiate same class over and over again
 #BAISCALLY JUST COPY AND PASTED HERE, MAKE SURE THESE TESTS ARE RELEVANT TO INSERTION
-  def test_if_sorter_is_a_new_instance_of_insertSort_class
-    sorter = insertSort.new
-    assert_equal insertSort, sorter.class
+  def test_if_sorter_is_a_new_instance_of_insertionSort_class
+    sorter = InsertionSort.new
+    assert_equal InsertionSort, sorter.class
   end
 
   def test_if_empty_array_passed_through
     skip
-    sorter = insertSort.new([])
-    assert_equal nil,sorter.sort
+    sorter = InsertionSort.new
+    assert_equal [],sorter.sort([])
   end
 
   def test_if_one_single_element_in_array
-    skip
-    sorter = insertSort.new([1])
-    assert_equal [1], sorter.sort
+
+    sorter = InsertionSort.new
+    assert_equal [1], sorter.sort([1])
   end
 
   def test_sort_on_array_with_several_chars
-    skip
-    sorter = insertSort.new(["d", "b", "a", "c"])
-    assert_equal ["a", "b", "c", "d"], sorter.sort
+
+    sorter = InsertionSort.new
+    assert_equal ["a", "b", "c", "d"], sorter.sort(["d", "b", "a", "c"])
   end
 
   def test_sort_on_array_with_several_integers
-    skip
-    sorter = insertSort.new(["4", "2", "1", "3"])
-    assert_equal ["1", "2", "3", "4"], sorter.sort
-  end
-
-  def test_if_array_unsortable_due_to_many_types
-    skip
-    #someone tries to pass in a string and an integer at same time?
-    sorter = insertSort.new
-    assert_equal error(or something else here??), sorter.sort
+    
+    sorter = InsertionSort.new
+    assert_equal ["1", "2", "3", "4"], sorter.sort(["4", "2", "1", "3"])
   end
 
 end
