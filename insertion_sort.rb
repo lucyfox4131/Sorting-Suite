fb23063d06e68b024b9dcef9244d9e474dc5d687
Lucy Fox <lucyreneefox@gmail.com>
2016-04-03 23:34:26 -0600

    insertion working

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,19 +1,32 @@
 class InsertionSort
 
-  def initialize
-    @array = []
-  end
 
-  def sort(array = @array)
-    new_array = array.shift
-    
+  def sort(array)
+    sorted_array = []
+    if array.empty?
+      return sorted_array
+    else
+      for element in array
+        if sorted_array.empty?
+          sorted_array << array[0]
+        else
+          sorted_array_length = sorted_array.length
+          insert_index = -1
+
+          (0...sorted_array_length).each do |i|
+            if element <= sorted_array[i]
+              insert_index = i
+              break
+            end
+          end
+          sorted_array.insert( insert_index ,element)
+        end
+      end
+    end
+    return sorted_array
   end
 
 end
 
-
-
-
-#very first thing I am doing is taking the first element
-#in the array given and putting it into our new array,
-#now I'll have to loop through
+sorter = InsertionSort.new
+p sorter.sort([5,7,3,6,2,4,1,10, 0])
--- insertion_sort_test.rb
+++ insertion_sort_test.rb
@@ -4,15 +4,13 @@ require './insertion_sort'
 
 class InsertionSortTest < Minitest::Test
 
-#get tips on pulling out a helper class for testing so don't always have to instantiate same class over and over again
-#BAISCALLY JUST COPY AND PASTED HERE, MAKE SURE THESE TESTS ARE RELEVANT TO INSERTION
   def test_if_sorter_is_a_new_instance_of_insertionSort_class
     sorter = InsertionSort.new
     assert_equal InsertionSort, sorter.class
   end
 
   def test_if_empty_array_passed_through
-    skip
+
     sorter = InsertionSort.new
     assert_equal [],sorter.sort([])
   end
@@ -30,7 +28,7 @@ class InsertionSortTest < Minitest::Test
   end
 
   def test_sort_on_array_with_several_integers
-    
+
     sorter = InsertionSort.new
     assert_equal ["1", "2", "3", "4"], sorter.sort(["4", "2", "1", "3"])
   end
