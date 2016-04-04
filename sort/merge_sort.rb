7520e8c04eb8239b2f8c986366ba744887cd2179
Lucy Fox <lucyreneefox@gmail.com>
2016-04-04 00:56:10 -0600

    merge sort working

--- sort/merge_sort.rb
+++ sort/merge_sort.rb
@@ -1,3 +1,52 @@
 class MergeSort
 
+  def merge_sort(array)
+    if array.length <= 1
+      array
+    else
+      midpoint = (array.length/2).floor
+      left = merge_sort(array[0..midpoint-1])
+      right = merge_sort(array[midpoint..array.length])
+      merge(left, right)
+    end
+  end
+
+
+  def merge(left = [], right = [])
+    sorted_array = []
+    if left.empty?
+      sorted_array + right
+    elsif right.empty?
+      sorted_array + left
+    elsif left.first < right.first
+      [left.first] + merge(left[1..left.length], right)
+    else
+      [right.first] + merge(left, right[1..right.length])
+    end
+  end
+
+
 end
+
+sorter = MergeSort.new
+p sorter.merge_sort([5,3,2,4,1])
+
+
+
+
+# def merge(left = [], right = [])
+#   sorted_array = []
+#   if left.empty?
+#     sorted_array + right
+#   end
+#   if right.empty?
+#     left
+#   end
+#   if left.first < right.first
+#     [left.first] + merge(left[1..left.length], right)
+#   elsif right.first < left.first
+#     [right.first] + merge(right[1..right.length], left)
+#   end
+#
+# end
+#
--- test/merge_sort_test.rb
+++ test/merge_sort_test.rb
@@ -3,5 +3,33 @@ require "minitest/pride"
 require "../sort/merge_sort"
 
 
-class insertSortTest < Minitest::Test
+class TestMergeSort < Minitest::Test
+  def test_if_sorter_is_a_new_instance_of_merge_class
+    sorter = MergeSort.new
+    assert_equal MergeSort, sorter.class
+  end
+
+  def test_if_empty_array_passed_through
+
+    sorter = MergeSort.new
+    assert_equal [],sorter.merge_sort([])
+  end
+
+  def test_if_one_single_element_in_array
+
+    sorter = MergeSort.new
+    assert_equal [1], sorter.merge_sort([1])
+  end
+
+  def test_sort_on_array_with_several_chars
+
+    sorter = MergeSort.new
+    assert_equal ["a", "b", "c", "d"], sorter.merge_sort(["d", "b", "a", "c"])
+  end
+
+  def test_sort_on_array_with_several_integers
+
+    sorter = MergeSort.new
+    assert_equal ["1", "2", "3", "4"], sorter.merge_sort(["4", "2", "1", "3"])
+  end
 end
